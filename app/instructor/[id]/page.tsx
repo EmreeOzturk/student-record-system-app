3d95972ab4faccfef683a1c047384e526611ead5
Emre <emreeozturk42@gmail.com>
2023-05-31 16:11:43 +0300

    fixed error 5

--- app/instructor/[id]/page.tsx
+++ app/instructor/[id]/page.tsx
@@ -1,6 +1,29 @@
 import InstructorPageContainer from '@/containers/InstructorPageContainer';
 const API_PREFIX = 'http://localhost:3000/api';
 
+const fetchInstructorInfo = async (id: string) => {
+  const res = await fetch(`${API_PREFIX}/getInstructorInfo/${id}`, {
+    cache: 'no-cache',
+  });
+  const data = await res.json();
+  return data;
+};
+
+const fetchInstructorCourses = async (id: string) => {
+  const res = await fetch(`${API_PREFIX}/getInstructorCourses/${id}`, {
+    cache: 'no-cache',
+  });
+  const data = await res.json();
+  return data;
+};
+
+const fetchInstructorEvents = async () => {
+  const res = await fetch(`${API_PREFIX}/getEvents`, {
+    cache: 'no-cache',
+  });
+  const data = await res.json();
+  return data;
+};
 
 const InstructorPage = async ({
   params,
@@ -9,23 +32,18 @@ const InstructorPage = async ({
     id: string;
   };
 }) => {
+  const info = fetchInstructorCourses(params.id);
+  const courses = fetchInstructorCourses(params.id);
+  const events = fetchInstructorEvents();
 
-
-  const [info, events, courses] = await Promise.all([
-    fetch(`${API_PREFIX}/getInstructorInfo/${params.id}`, {
-      cache: 'no-cache',
-    }).then((res) => res.json()),
-    fetch(`${API_PREFIX}/getEvents`, {
-      cache: 'no-cache',
-    }).then((res) => res.json()),
-    fetch(`${API_PREFIX}/getInstructorCourses/${params.id}`, {
-      cache: 'no-cache',
-    }).then((res) => res.json()),
+  const [infodata, coursesdata, eventsdata] = await Promise.all([
+    info,
+    courses,
+    events,
   ]);
 
   return (
     <div>
-      
       <h1>asdasdasdsa</h1>
     </div>
   );
