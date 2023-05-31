fcd09c8607ce28f8781f04c366048707b5f7e030
Emre <emreeozturk42@gmail.com>
2023-05-31 16:08:19 +0300

    error fixed 4

--- app/instructor/[id]/page.tsx
+++ app/instructor/[id]/page.tsx
@@ -11,6 +11,17 @@ const InstructorPage = async ({
 }) => {
 
 
+  const [info, events, courses] = await Promise.all([
+    fetch(`${API_PREFIX}/getInstructorInfo/${params.id}`, {
+      cache: 'no-cache',
+    }).then((res) => res.json()),
+    fetch(`${API_PREFIX}/getEvents`, {
+      cache: 'no-cache',
+    }).then((res) => res.json()),
+    fetch(`${API_PREFIX}/getInstructorCourses/${params.id}`, {
+      cache: 'no-cache',
+    }).then((res) => res.json()),
+  ]);
 
   return (
     <div>
